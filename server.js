0dc021bad66a3ca90c965c6e975d49812905c747
Reed <reeed7@gmail.com>
2016-08-21 19:21:32 +0000

    ver1

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
 var express = require("express");
 var app = express();
 
-app.get('/', (req, resp)=>{
-    resp.end("Hello World!");
+app.get(/^\/\d+$/, (req, resp)=>{
+    resp.end(printDate(+req.url.substr(1)));
+    
+}).get(/^\/\w+\%20\d{1,2},\%20[1-9]\d{3}$/, (req, resp)=>{
+    resp.end(printDate(req.url.substr(1)));
+    
 }).listen(8080, ()=>{
     console.log("Server started!");
 });
+
+function printDate(dateStrOrTimestamp) {
+    if('string' === typeof dateStrOrTimestamp) {
+        dateStrOrTimestamp = decodeURIComponent(dateStrOrTimestamp);
+    }
+    
+    var dateObj = {unix:'', natural:''};
+    var months = ['Janurary', 'Feburary', 'March', 'April', 
+                    'May', 'June', 'July', 'August', 'September', 
+                    'October', 'November', 'December'];
+    
+    var date = new Date(dateStrOrTimestamp);
+    
+    dateObj.unix = date.getTime();
+    dateObj.natural = months[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
+    
+    return JSON.stringify(dateObj);
+}
